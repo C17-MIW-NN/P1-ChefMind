9251545637a8a7b656ba93717fa9a19056bc13ab
Assib <assibpajman@gmail.com>
2025-11-10 11:50:30 +0100

    first commit maybe second

---
+++ src/main/java/nl/miwnn/ch17/codalabs/chefmind/ChefMindApplication.java
@@ -0,0 +1,13 @@
+package nl.miwnn.ch17.codalabs.chefmind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChefMindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChefMindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/miwnn/ch17/codalabs/chefmind/ChefMindApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwnn.ch17.codalabs.chefmind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChefMindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

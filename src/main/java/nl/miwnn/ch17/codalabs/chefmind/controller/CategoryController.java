79a0759603ff960747aa6b09574f69b335d8b7bb
harmvdweide <harmvdweide@hotmail.com>
2025-11-12 12:42:55 +0100

    categoryForm.html is ready

--- src/main/java/nl/miwnn/ch17/codalabs/chefmind/controller/CategoryController.java
+++ src/main/java/nl/miwnn/ch17/codalabs/chefmind/controller/CategoryController.java
@@ -1,12 +1,15 @@
 package nl.miwnn.ch17.codalabs.chefmind.controller;
 
 import nl.miwnn.ch17.codalabs.chefmind.model.Category;
+import nl.miwnn.ch17.codalabs.chefmind.model.Recipe;
 import nl.miwnn.ch17.codalabs.chefmind.repositories.CategoryRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 /**
  * @author Harm van der Weide
@@ -23,15 +26,42 @@ public class CategoryController {
     }
 
     @GetMapping("/all")
-    private String showCategoryOverview (Model datamodel){
+    private String showCategoryOverview (Model datamodel, Category category){
         datamodel.addAttribute("allCategories", categoryRepository.findAll());
+        datamodel.addAttribute("newCategory", new Category());
 
         return "categoryOverview";
     }
 
-    @PostMapping("/add")
-    private String showCategoryForm (Model datamodel){
-        datamodel.addAttribute("newCategory", new Category());
+    @GetMapping("/add")
+    public String showCategoryForm(Model datamodel) {
+        return showCategoryForm(datamodel, new Category());
+    }
+
+    @PostMapping("/save")
+    public String saveOrUpdateCategory (@ModelAttribute("formCategory") Category categoryToBeSaved,
+                                     BindingResult result,
+                                     Model datamodel) {
+        Optional<Category> categoryWithSameName =
+                categoryRepository.findByCategoryName(categoryToBeSaved.getCategoryName());
+
+        if (categoryWithSameName.isPresent() &&
+                !categoryWithSameName.get().getCategoryId().equals(categoryToBeSaved.getCategoryId())) {
+            result.addError(new FieldError("Category",
+                    "name",
+                    "Category name already exists"));
+        }
+
+        if (result.hasErrors()) {
+            return showCategoryForm(datamodel, categoryToBeSaved);
+        }
+
+        categoryRepository.save(categoryToBeSaved);
+        return "redirect:/category/all";
+    }
+
+    public String showCategoryForm(Model datamodel, Category category) {
+        datamodel.addAttribute("formCategory", category);
 
         return "categoryForm";
     }

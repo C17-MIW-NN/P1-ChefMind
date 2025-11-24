494a2089da19948290b29f096250a152f6056a8f
Assib <assibpajman@gmail.com>
2025-11-24 10:10:01 +0100

    Enabled and configured security (no users)

---
+++ src/main/java/nl/miwnn/ch17/codalabs/chefmind/config/ChefMindSecurityConfig.java
@@ -0,0 +1,33 @@
+package nl.miwnn.ch17.codalabs.chefmind.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * @author Assib Pajman
+ *
+ */
+@Configuration
+@EnableWebSecurity
+public class ChefMindSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests((request) -> request
+                        .requestMatchers("/", "/recipe/all", "recipe/detail/*").permitAll()
+                        .requestMatchers("/category/*", "/author/details/*", "/image/**").permitAll()
+                        .requestMatchers("/css/**", "/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .logout((logout) -> logout.logoutSuccessUrl("/"))
+        ;
+
+        return httpSecurity.build();
+    }
+}
